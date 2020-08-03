dba6a5092242f9ce691df8eb265337afd1e41db2
nikhildodeja <nikhildodeja91@gmail.com>
2020-08-03 18:06:41 +0530

    ver

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const printMessage = function (str) {
     console.log(`Hello -- ${str}`);
 }
 
+const printHello = function () {
+    console.log('Hello');
+}
 
 module.exports = {
-    printMessage
+    printMessage,
+    printHello
 }
 
